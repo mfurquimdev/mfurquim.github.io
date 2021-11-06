a8eaad99748c6c252356d64c1cab24f299dd42f0
Mateus Furquim <furquim@devgrid.co.uk>
2021-11-06 19:41:55 -0300

    Add googly eyes on top

--- googly_eyes.js
+++ googly_eyes.js
@@ -11,7 +11,8 @@ var input = {
   },
   mouseY: {
     start: 0,
-    end: window.innerHeight,
+    end: 70,
+//    end: window.innerHeight,
     current: 0,
   },
 };
@@ -22,13 +23,13 @@ input.mouseY.range = input.mouseY.end - input.mouseY.start;
 // output
 var output = {
   x: {
-    start: -70,
-    end: 70,
+    start: 0,
+    end: 50,
     current: 0,
   },
   y: {
-    start: -100,// -70
-    end: 100,// 70
+    start: 0,
+    end: 50,
     current: 0,
 
   },
@@ -41,15 +42,20 @@ output.y.range = output.y.end - output.y.start;
 var handleMouseMove = function(event) {
   input.mouseX.current = event.clientX;
   input.mouseX.fraction = (input.mouseX.current - input.mouseX.start) / input.mouseX.range;
-  
+
   input.mouseY.current = event.clientY;
   input.mouseY.fraction = (input.mouseY.current - input.mouseY.start) / input.mouseY.range;
-  
+
   output.x.current = output.x.start + (input.mouseX.fraction * output.x.range);
   output.y.current = output.y.start + (input.mouseY.fraction * output.y.range);
   output.x.opposite = output.x.end - (input.mouseX.fraction * output.x.range);
   output.y.opposite = output.y.end - (input.mouseY.fraction * output.y.range);
-  
+
+  if (output.y.current >= output.x.end) {
+    output.y.current = output.x.end;
+  }
+
+
   pupilsArray.forEach(function (pupil, i) {
     if (i % 2) {
       pupil.style.transform = 'translate(' + output.x.current + 'px, ' + output.y.current + 'px)';
@@ -62,7 +68,7 @@ var handleMouseMove = function(event) {
 
 
 var handleTouchMove = function(event) {
-  
+
   console.log('event', event.touches[0].clientX)
 }
 
@@ -70,7 +76,7 @@ var handleTouchMove = function(event) {
 var handleResize = function(event) {
   input.mouseX.end = window.innerWidth - 200;
   input.mouseX.range = input.mouseX.end - input.mouseX.start;
-  
+
   input.mouseY.end = window.innerHeight - 75;
   input.mouseY.range = input.mouseY.end - input.mouseY.start;
   console.log('window size \(', window.innerWidth, ',', window.innerHeight, ')');
