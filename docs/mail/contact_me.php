5b417cc1d131dca8a05a2f5252e366d81d217152
Mateus Furquim <furquim@devgrid.co.uk>
2022-09-23 11:29:56 -0300

    Ajustando formulário de e-mail

--- docs/mail/contact_me.php
+++ docs/mail/contact_me.php
@@ -2,7 +2,6 @@
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
